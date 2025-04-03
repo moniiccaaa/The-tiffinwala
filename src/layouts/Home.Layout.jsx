8af833b3ff89dd693bf3479d15c7a802b54fdb7f
Mahima Pandey <mahi.x0704@gmail.com>
2025-04-04 01:02:54 +0530

    Avatar file  added in Navbar

---
+++ src/layouts/Home.Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar/Navbar'
+
+const HomeLayout = ({ children }) => {
+    return (
+      <div className="layout flex flex-col h-full overflow-x-hidden">
+        <Navbar />
+        {children} {/* This renders HomePage content */}
+        <Footer />
+      </div>
+    );
+  };
+  
+  export default HomeLayout;
+  
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store"; // Import store
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+
+
+
+
+// The Provider component from react-redux makes the store available to all components.
+
+// Now, any component can read from Redux state using useSelector() and update state using useDispatch().
